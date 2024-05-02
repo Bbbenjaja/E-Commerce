8af910d63c901a9a426271f5a16687813f352bb2
Benja <benjaminvalenzuela023@gmail.com>
2024-05-01 20:58:03 -0700

    Dependencies installed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.toolstodo.ecommerce"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.toolstodo.ecommerce"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +40,10 @@ android {
 
 dependencies {
 
+    val navVersion = "2.7.1"
+    val retrofitVersion = "2.9.0"
+    val hiltVersion = "2.44"
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -44,4 +51,30 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Navigation component
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    //ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    //LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    //Activity
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    //Dagger hilt
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+    //Picasso
+    implementation("com.squareup.picasso:picasso:2.8")
+    //Room
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 }
